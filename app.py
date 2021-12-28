7a7e3e54574c5e504a4a7e0d3f604801fa5cc0e8
YoonSoeHyeon <ysh0714@naver.com>
2021-12-28 23:32:34 +0900

    파일추가 및 커밋테스트

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def main():
+    return render_template("main.html")
+
+
+@app.route('/sign-in')
+def detail():
+    return render_template("sign-in.html")
+
+@app.route('/sign-up')
+def detail():
+    return render_template("sign-up.html")
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
