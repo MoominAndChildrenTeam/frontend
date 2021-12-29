a64bec23a595bb3f42fb13b6f71ee44bfddd95fd
YoonSoeHyeon <ysh0714@naver.com>
2021-12-29 23:59:41 +0900

    사진 업로드기능 완료

--- static/sign-up.js
+++ static/sign-up.js
@@ -1,17 +1,65 @@
-const browseBtn = document.querySelector('.browse_button');
-const realInput = document.querySelector('#image');
+function DropFile(dropAreaId, fileListId) {
+  let dropArea = document.getElementById(dropAreaId);
+  let fileList = document.getElementById(fileListId);
 
-browseBtn.addEventListener('click',()=>{
-	realInput.click();
-});
+  function preventDefaults(e) {
+    e.preventDefault();
+    e.stopPropagation();
+  }
 
-function setThumbnail(event) {
-	var reader = new FileReader();
-	reader.onload = function (event) {
-		var img = document.createElement("img");
-		img.setAttribute("src", event.target.result);
-		document.querySelector("div#preveiw_container").append(img);
-	};
-	reader.readAsDataURL(event.target.files[0]);
+  function highlight(e) {
+    preventDefaults(e);
+    dropArea.classList.add("highlight");
+  }
+
+  function unhighlight(e) {
+    preventDefaults(e);
+    dropArea.classList.remove("highlight");
+  }
+
+  function handleDrop(e) {
+    unhighlight(e);
+    let dt = e.dataTransfer;
+    let files = dt.files;
+
+    handleFiles(files);
+
+    const fileList = document.getElementById(fileListId);
+    if (fileList) {
+      fileList.scrollTo({ top: fileList.scrollHeight });
+    }
+  }
+
+  function handleFiles(files) {
+    files = [...files];
+    // files.forEach(uploadFile);
+    files.forEach(previewFile);
+  }
+
+  function previewFile(file) {
+    console.log(file);
+    renderFile(file);
+  }
+
+  function renderFile(file) {
+    let reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadend = function () {
+      let img = dropArea.getElementsByClassName("preview")[0];
+      img.src = reader.result;
+      img.style.display = "block";
+    };
+  }
+
+  dropArea.addEventListener("dragenter", highlight, false);
+  dropArea.addEventListener("dragover", highlight, false);
+  dropArea.addEventListener("dragleave", unhighlight, false);
+  dropArea.addEventListener("drop", handleDrop, false);
+
+  return {
+    handleFiles
+  };
 }
+
+const dropFile = new DropFile("drop-file", "files");
 
