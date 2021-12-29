af31f1788ebbddfae9aec06c73e3f0fce5661754
YoonSoeHyeon <ysh0714@naver.com>
2021-12-29 21:57:09 +0900

    회원가입파일 수정중

---
+++ static/sign-up.js
@@ -0,0 +1,17 @@
+const browseBtn = document.querySelector('.browse_button');
+const realInput = document.querySelector('#image');
+
+browseBtn.addEventListener('click',()=>{
+	realInput.click();
+});
+
+function setThumbnail(event) {
+	var reader = new FileReader();
+	reader.onload = function (event) {
+		var img = document.createElement("img");
+		img.setAttribute("src", event.target.result);
+		document.querySelector("div#preveiw_container").appendChild(img);
+	};
+	reader.readAsDataURL(event.target.files[0]);
+}
+
