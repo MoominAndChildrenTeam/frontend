998ca68c8f40b38c72f0277cb5691d07c54989ca
이병준 <yya71bb@naver.com>
2021-12-31 17:11:48 +0900

    작성중입니다.

--- static/mypage.js
+++ static/mypage.js
@@ -18,13 +18,20 @@ function get_mypage(){
                 data: {},
                 success: function (response) {
                         let name = ['name']
-                        let follower = ['follower']
-                        let following = ['following']
+                        let feed_count = ['feed_count']
+                        let follower_count = ['follower']
+                        let following_count = ['following']
                         let profile_img =
 
                         let temp_name = `<b>${name}</b>`
-                        let temp_wrapper = ``
+                        let temp_feed_count = `<b>${feed_count}</b>`
+                        let temp_follower_count = `<b>${follower_count}</b>`
+                        let temp_following_count = `<b>${following_count}</b>`
+
                         $('#nickname').append(temp_name)
+                        $('#feed').append(temp_feed_count)
+                        $('#follower_count').append(temp_follower_count)
+                        $('#following_count').append(temp_following_count)
 
                 }
             });
