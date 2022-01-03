7c20d8481756743f6b261f6019f978c501b02856
이병준 <yya71bb@naver.com>
2022-01-03 10:00:48 +0900

    작성중입니다.

--- my_page/static/my_page.js
+++ my_page/static/my_page.js
@@ -21,25 +21,30 @@ function get_mypage(){
                         let feed_count = ['feed_count']
                         let follower_count = ['follower']
                         let following_count = ['following']
-                        let newimage = document.createElement('img');
-                        newimage.setAttribute('class','img')
-                        newimage.src = ['image']
-                        newimage.style.borderRadius : 50%;
-                        newimage.style.height : 20vw;
-                        newimage.style.width : 20vw;
+                        let new_profile_image = document.createElement('profile_img');
+                        let new_footer_image = document.createElement('footer_img');
+
+                        new_profile_image.setAttribute('class', my-page-img1)
+                        new_profile_image.src = ['image']
+
+                        new_footer_image.setAttribute('class',my-page-img)
+                        new_footer_image.src = ['image']
+
+                        let introduce = ['introduce']
                         let container = document.getElementById('#profile-image');
                         container.append(newImage);
 
+                        let temp_introduce = `<p style="margin-top: 0">${introduce}</p>`
                         let temp_name = `<b>${name}</b>`
                         let temp_feed_count = `<b>${feed_count}</b>`
                         let temp_follower_count = `<b>${follower_count}</b>`
                         let temp_following_count = `<b>${following_count}</b>`
 
+                        $('#introduce').append(temp_introduce)
                         $('#nickname').append(temp_name)
                         $('#feed').append(temp_feed_count)
                         $('#follower_count').append(temp_follower_count)
                         $('#following_count').append(temp_following_count)
-
                 }
             });
 }
